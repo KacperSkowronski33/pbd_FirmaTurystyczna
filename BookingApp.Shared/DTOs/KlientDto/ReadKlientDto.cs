0408c7198a4975a5f2ecd14b4032e62f00cd1187
Krzysztof Bierzanek <krzysztof.bierzanek@gmail.com>
2026-04-12 23:01:18 +0200

    klient (controler dto dziala)

---
+++ BookingApp.Shared/DTOs/KlientDto/ReadKlientDto.cs
@@ -0,0 +1,21 @@
+﻿using BookingApp.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Shared.DTOs.KlientDto
+{
+    public class ReadKlientDto
+    {
+        public int Id { get; set; }
+        public string Imie { get; set; } = string.Empty;
+        public string Nazwisko { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Haslo { get; set; } = string.Empty;
+        public string? AdresKlienta { get; set; }
+
+        public ICollection<Rezerwacja> Rezerwacje = new List<Rezerwacja>();
+    }
+}
