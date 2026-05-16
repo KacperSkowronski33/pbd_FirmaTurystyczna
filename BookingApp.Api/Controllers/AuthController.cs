411da286f1f6e5d2bfd1e2b6e2a34c7a41d5f6ca
JuraszekCPP <ij316297@student.polsl.pl>
2026-05-16 18:54:11 +0200

    dziala teraz 100%
    s

--- BookingApp.Api/Controllers/AuthController.cs
+++ BookingApp.Api/Controllers/AuthController.cs
@@ -33,50 +33,52 @@ namespace BookingApp.Api.Controllers
             TypRoli rola = TypRoli.Brak;
             string userMail = "";
             string userName = "";
+            int userId = 0;
 
             if (!loginDto.Identifier.Contains("@"))
             {
                 var pracownik = await _context.Pracownicy
                     .Include(p => p.Rola)
                     .FirstOrDefaultAsync(p => p.Email == loginDto.Identifier);
-                if (pracownik == null || !BCrypt.Net.BCrypt.Verify(loginDto.Haslo, pracownik.Haslo))
+
+                if (pracownik == null || pracownik.Haslo != loginDto.Haslo)
                 {
-                    return Unauthorized(new LoginResultDto { Success = false, Wiadomosc = "Niepoprawny login lub hasło"});
+                    return Unauthorized(new LoginResultDto { Success = false, Wiadomosc = "Niepoprawny login lub hasło" });
                 }
                 rola = pracownik.Rola.TypRoli;
                 userMail = pracownik.Email;
                 userName = $"{pracownik.Imie} {pracownik.Nazwisko}";
-            } else
+                userId = pracownik.Id;
+            }
+            else
             {
                 var klient = await _context.Klienci
                     .FirstOrDefaultAsync(k => k.Email == loginDto.Identifier);
 
-                if(klient == null || !BCrypt.Net.BCrypt.Verify(loginDto.Haslo, klient.Haslo))
-                { 
+                if (klient == null || klient.Haslo != loginDto.Haslo)
+                {
                     return Unauthorized(new LoginResultDto { Success = false, Wiadomosc = "Niepoprawny login lub hasło" });
                 }
 
                 rola = TypRoli.Brak;
                 userMail = klient.Email;
                 userName = $"{klient.Imie} {klient.Nazwisko}";
-
+                userId = klient.Id;
             }
 
-            var token = GenJwtToken(userMail, rola, userName);
+            var token = GenJwtToken(userMail, rola, userName, userId);
             return Ok(new LoginResultDto { Success = true, Token = token });
-
         }
 
-        private string GenJwtToken(string Mail, TypRoli Rola, string Name)
+        private string GenJwtToken(string Mail, TypRoli Rola, string Name, int Id)
         {
-
             var claims = new List<Claim>
-            {
-
-                new Claim(ClaimTypes.Name, Name),
-                new Claim(ClaimTypes.Email, Mail),
-                new Claim(ClaimTypes.Role, Rola.ToString()),
-            };
+    {
+        new Claim(ClaimTypes.NameIdentifier, Id.ToString()),
+        new Claim(ClaimTypes.Name, Name),
+        new Claim(ClaimTypes.Email, Mail),
+        new Claim(ClaimTypes.Role, Rola.ToString()),
+    };
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
@@ -88,7 +90,8 @@ namespace BookingApp.Api.Controllers
                 claims,
                 expires: expires,
                 signingCredentials: creds
-                );
+            );
+
             return new JwtSecurityTokenHandler().WriteToken(Token);
         }
     }
